8217c408cc744bf2fc2be4c151d3b3debf6fb69d
PhamVietHau <phamviethau27112003@gmail.com>
2025-11-05 22:35:20 +0700

    Initialize repo

---
+++ src/main/java/HelloController.java
@@ -0,0 +1,13 @@
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello/v1")
+public class HelloController {
+    @GetMapping
+    public ResponseEntity<String> hello(){
+        return  ResponseEntity.ok("Hello world");
+    }
+}
---
+++ src/test/java/com/example/helloworld_deloy/HelloWorldDeloyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworld_deloy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldDeloyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
