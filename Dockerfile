59b3166fd0c7f71b20c42d373c55ae40660b6750
PhamVietHau <phamviethau27112003@gmail.com>
2025-11-06 12:12:27 +0700

    Commit Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,13 @@
-FROM
+FROM eclipse-temurin:17-jdk-jammy
+
+# Tạo thư mục làm việc trong container
+WORKDIR /app
+
+# Sao chép file JAR vào container
+COPY target/*.jar app.jar
+
+# Mở cổng 8080 (hoặc cổng bạn dùng)
+EXPOSE 8090
+
+# Lệnh chạy ứng dụng
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/example/helloworld_deloy/controller/HelloController.java
+++ src/main/java/com/example/helloworld_deloy/controller/HelloController.java
@@ -1,3 +1,5 @@
+package com.example.helloworld_deloy.controller;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello/v1")
 public class HelloController {
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<String> hello(){
         return  ResponseEntity.ok("Hello world");
     }
